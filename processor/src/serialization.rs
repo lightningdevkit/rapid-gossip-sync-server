6935a665824caa3e987370651c8a503702522ac6
Arik Sosman <git@arik.io>
2022-07-01 17:41:49 -0700

    simplify serialization logic pertaining to intermediate update checks

--- processor/src/serialization.rs
+++ processor/src/serialization.rs
@@ -160,34 +160,33 @@ pub(super) fn serialize_delta_set(delta_set: DeltaSet, last_sync_timestamp: u32,
 					if let Some(last_seen_update) = updates.last_update_before_seen {
 
 						// we typically compare only the latest update with the last seen
-						let mut compared_updates = vec![last_seen_update];
+						let mut compared_updates = vec![latest_update.clone()];
 						if consider_intermediate_updates && !updates.intermediate_updates.is_empty() {
 							// however, if intermediate updates are to be considered,
 							// they are all included
+							// the order of the updates doesn't matter because we compare all
+							// changes solely against the originally known value
 							compared_updates.append(&mut updates.intermediate_updates.clone());
 						}
-						compared_updates.push(latest_update.clone());
 
 						let mut mutated_properties = MutatedProperties::default();
-						for i in 1..compared_updates.len() {
-							let previous_update = &compared_updates[i - 1];
-							let current_update = &compared_updates[i];
-							if current_update.flags != previous_update.flags {
+						for current_update in compared_updates {
+							if current_update.flags != last_seen_update.flags {
 								mutated_properties.flags = true;
 							}
-							if current_update.cltv_expiry_delta != previous_update.cltv_expiry_delta {
+							if current_update.cltv_expiry_delta != last_seen_update.cltv_expiry_delta {
 								mutated_properties.cltv_expiry_delta = true;
 							}
-							if current_update.htlc_minimum_msat != previous_update.htlc_minimum_msat {
+							if current_update.htlc_minimum_msat != last_seen_update.htlc_minimum_msat {
 								mutated_properties.htlc_minimum_msat = true;
 							}
-							if current_update.fee_base_msat != previous_update.fee_base_msat {
+							if current_update.fee_base_msat != last_seen_update.fee_base_msat {
 								mutated_properties.fee_base_msat = true;
 							}
-							if current_update.fee_proportional_millionths != previous_update.fee_proportional_millionths {
+							if current_update.fee_proportional_millionths != last_seen_update.fee_proportional_millionths {
 								mutated_properties.fee_proportional_millionths = true;
 							}
-							if current_update.htlc_maximum_msat != previous_update.htlc_maximum_msat {
+							if current_update.htlc_maximum_msat != last_seen_update.htlc_maximum_msat {
 								mutated_properties.htlc_maximum_msat = true;
 							}
 						};

d36993aaefc84b498cbc2348052eec6c8a444836
Arik Sosman <git@arik.io>
2023-08-15 14:38:17 -0700

    Create TestLogger.

--- src/types.rs
+++ src/types.rs
@@ -36,3 +36,58 @@ impl Logger for RGSSLogger {
 		println!("{:<5} [{} : {}, {}] {}", record.level.to_string(), record.module_path, record.file, record.line, record.args);
 	}
 }
+
+#[cfg(test)]
+pub mod tests {
+	use std::collections::HashMap;
+	use std::sync::{Mutex};
+	use lightning::util::logger::{Level, Logger, Record};
+
+	pub struct TestLogger {
+		level: Level,
+		pub(crate) id: String,
+		pub lines: Mutex<HashMap<(String, String), usize>>,
+	}
+
+	impl TestLogger {
+		pub fn new() -> TestLogger {
+			Self::with_id("".to_owned())
+		}
+		pub fn with_id(id: String) -> TestLogger {
+			TestLogger {
+				level: Level::Trace,
+				id,
+				lines: Mutex::new(HashMap::new()),
+			}
+		}
+		pub fn enable(&mut self, level: Level) {
+			self.level = level;
+		}
+		pub fn assert_log(&self, module: String, line: String, count: usize) {
+			let log_entries = self.lines.lock().unwrap();
+			assert_eq!(log_entries.get(&(module, line)), Some(&count));
+		}
+
+		/// Search for the number of occurrence of the logged lines which
+		/// 1. belongs to the specified module and
+		/// 2. contains `line` in it.
+		/// And asserts if the number of occurrences is the same with the given `count`
+		pub fn assert_log_contains(&self, module: &str, line: &str, count: usize) {
+			let log_entries = self.lines.lock().unwrap();
+			let l: usize = log_entries.iter().filter(|&(&(ref m, ref l), _c)| {
+				m == module && l.contains(line)
+			}).map(|(_, c)| { c }).sum();
+			assert_eq!(l, count)
+		}
+	}
+
+	impl Logger for TestLogger {
+		fn log(&self, record: &Record) {
+			*self.lines.lock().unwrap().entry((record.module_path.to_string(), format!("{}", record.args))).or_insert(0) += 1;
+			if record.level >= self.level {
+				// #[cfg(feature = "std")]
+				println!("{:<5} {} [{} : {}, {}] {}", record.level.to_string(), self.id, record.module_path, record.file, record.line, record.args);
+			}
+		}
+	}
+}
